4a804e7f4c164baf1bb9247908092e553b755c6b
Ashley Mains <amains@rsglabs.com>
2020-04-03 16:16:13 -0400

    adding empty files for practice

---
+++ sessions/04-object-oriented-2/developer.php
@@ -0,0 +1,9 @@
+<?php
+// have a parent for user that provides child overridden method for a developer
+
+//challenge: Extend the User class for another type of user
+
+//challenges: Add an Interface
+//Change to User class to an abstract class
+//Define an abstract method
+?>
---
+++ sessions/04-object-oriented-2/sample-code/classes/job.php
@@ -0,0 +1,8 @@
+<?
+ declare(strict_types=1);
+ interface Job{
+     public function getJobTitle(): String;
+     public function setJobTitle(): String;
+ }
+
+?>
